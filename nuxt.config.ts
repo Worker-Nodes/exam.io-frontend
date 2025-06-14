f8a25febde0953256360de384dbfae8bca580561
Alexandre Mottier <alex_mottier@hotmail.com>
2025-06-14 19:01:34 +0200

    Add dev dependencies
    Signed-off-by: Alexandre Mottier <alex_mottier@hotmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+
+  modules: [
+    '@pinia/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/ui'
+  ]
 })
